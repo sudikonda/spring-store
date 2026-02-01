ded08f1f136a67762e3a343f41cff250e36463cc
sudikonda <sudikonda.sivakumar@gmail.com>
2026-02-01 11:22:15 -0500

    Initial commit

---
+++ src/test/java/com/me/store/SpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
